1cfc875313ad580e834a56aa7e4de1e081fb8d60
Shubham Singh <coolsks94@gmail.com>
2018-10-08 19:50:59 +0530

    DUTweaks: Allow users to switch between Pie/Oreo recents [2/3]
    Change-Id: Ic65c2e5d2c7dae8527e2261af6942e3ae1c0d959
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/Recents.java
+++ src/com/dirtyunicorns/tweaks/fragments/Recents.java
@@ -16,25 +16,52 @@
 package com.dirtyunicorns.tweaks.fragments;
 
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
+
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
-import com.dirtyunicorns.support.preferences.IconPackPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Recents extends SettingsPreferenceFragment implements Indexable {
 
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final int RECENTS_COMPONENT_OREO = 1;
+
+    private ListPreference mRecentsComponentType;
+    private PreferenceCategory oreoRecentsCat;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.recents);
+
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
+
+        oreoRecentsCat = (PreferenceCategory)findPreference("recents_ui_oreo_recents_category");
+        oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
     }
 
     @Override
@@ -43,6 +70,24 @@ public class Recents extends SettingsPreferenceFragment implements Indexable {
         IconPackPreference iconPackPref = (IconPackPreference) findPreference("recents_icon_pack");
         // Re-initialise preference
         iconPackPref.init();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) newValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
+            if (type == RECENTS_COMPONENT_OREO) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+        return true;
+        }
+      return false;
     }
 
     @Override
