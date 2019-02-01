4f79bf464d43073b587f7be8cd00125a5c2bda9d
Alex <du.alexcruz@gmail.com>
2019-01-31 19:17:58 -0600

    DUTweaks: Set a text length on the carrier label
    Up til now this has been gone unnoticed. Without a text limit the user
    could very easily blow up the statusbar to the point that is not usable.
    
    Change-Id: I2813af9e12c6d29e020669e0626faec8422fedbe
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/CarrierLabel.java
+++ src/com/dirtyunicorns/tweaks/fragments/CarrierLabel.java
@@ -34,6 +34,7 @@ import android.telephony.TelephonyManager;
 import android.text.format.DateFormat;
 import android.text.Spannable;
 import android.text.TextUtils;
+import android.text.InputFilter;
 import android.widget.EditText;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -147,7 +148,9 @@ public class CarrierLabel extends SettingsPreferenceFragment
             alert.setMessage(R.string.custom_carrier_label_explain);
             // Set an EditText view to get user input
             final EditText input = new EditText(getActivity());
+            int maxLength = 25;
             input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
             input.setSelection(input.getText().length());
             alert.setView(input);
             alert.setPositiveButton(getString(android.R.string.ok),
