d7fafbfdc1c2b35470da4ec31931ab7a629a24bf
Genkzsz11 <Genkzsz11@gmail.com>
2020-03-11 22:02:51 +0700

    dd

--- src/com/dirtyunicorns/tweaks/DirtyTweaks.java
+++ src/com/dirtyunicorns/tweaks/DirtyTweaks.java
@@ -60,11 +60,6 @@ import java.util.ArrayList;
 import java.util.List;
 import android.text.TextUtils;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
